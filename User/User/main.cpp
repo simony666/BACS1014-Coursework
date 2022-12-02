9c863a9de07351238592cc96f0bb874fc2132c70
WhiteHappy <simonyong1079@gmail.com>
2022-12-02 13:03:15 +0800

    Create Main file

---
+++ User/User/main.cpp
@@ -0,0 +1,20 @@
+/*
+* BACS1014 Assignment
+* Member : Yong Choy Mun
+*/
+
+#include <iostream>
+#include "function.h"
+
+using namespace std;
+
+int main() {
+	//Welcome Screen
+
+	//Enter Detail ( ic,student id)
+	//validation
+	//cout << validate("011111", "22WMR11111") << endl;
+	//cout << validate("011111", "22WMR11113") << endl;
+	//cout << validate("011113", "22WMR11113") << endl;
+	return 0;
+}

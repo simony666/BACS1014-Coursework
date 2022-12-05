da76c5c7e72a3a246b3c7355a3db65d4f8507ead
WhiteHappy <simonyong1079@gmail.com>
2022-12-05 13:16:27 +0800

    Create main.cpp

---
+++ coursework/coursework/main.cpp
@@ -0,0 +1,15 @@
+/*
+* BACS1014 Assignment
+* Member : Yong Choy Mun, Wang Siew You, Eddie Chua Kok Seng
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//Welcome Screen
+
+	//Enter Detail ( ic,student id)
+	return 0;
+}

164cb8b5157dd50e71c3cb7aacf93e212babac9d
Siewyou <119652381+Siewyou@users.noreply.github.com>
2022-12-29 14:26:39 +0800

    Update adminfunction.cpp

--- coursework/coursework/adminfunction.cpp
+++ coursework/coursework/adminfunction.cpp
@@ -53,7 +53,83 @@ int main()
     return 0;
 }
 
+Register student
+#include <iostream>
+#include <string>
+using namespace std;
 
+using namespace std;
+
+const int MAX_STUDENTS = 100;  // Maximum number of students that can be registered
+
+struct Student {
+    string name;
+    string mykad;
+    string password;
+    string studentid;
+};
+
+// Global array to store the registered students
+Student students[MAX_STUDENTS];
+int numStudents = 0;  // Number of students currently registered
+
+// Function to register a new student
+void registerStudent() {
+    if (numStudents >= MAX_STUDENTS) {
+        cout << "Error: Maximum number of students already registered." << endl;
+        return;
+    }
+
+    cin.ignore();
+    cout << "Enter name: ";
+    getline(cin,students[numStudents].name);
+//cin.getline(input,sizeof(input));
+
+    cout << "Enter MyKad Number: ";
+    getline(cin,students[numStudents].mykad);
+
+    cout << "Enter student ID: ";
+    getline(cin,students[numStudents].studentid);
+
+    numStudents++;
+    cout << "Student successfully registered." << endl;
+}
+
+// Function to display a list of all registered students
+void displayStudents() {
+    cout << "Registered students:" << endl;
+    for (int i = 0; i < numStudents; i++) {
+        cout << students[i].name << " (" << students[i].studentid << ")" << endl;
+    }
+}
+
+int main() {
+    while (true) {
+        cout << "Welcome to Administrator Level" << endl;
+        cout << "1. Register a new student" << endl;
+        cout << "2. Display all registered students" << endl;
+        cout << "3. Quit" << endl;
+        cout << "Enter your choice: ";
+
+        int choice;
+        cin >> choice;
+
+        if (choice == 1) {
+            registerStudent();
+        }
+        else if (choice == 2) {
+            displayStudents();
+        }
+        else if (choice == 3) {
+            break;
+        }
+        else {
+            cout << "Invalid choice. Try again." << endl;
+        }
+    }
+
+    return 0;
+}
 
 //Show voting and winner
 #include <iostream>
@@ -151,88 +227,6 @@ void displayResults(const map<string, int>& votes) {
         cout << "Another voting session will be again by another time." << endl;
         cout << "The time will determine by administrators." << endl;
     }
-}
-
-
-
-Register student
-#include <iostream>
-#include <string>
-using namespace std;
-
-using namespace std;
-
-const int MAX_STUDENTS = 100;  // Maximum number of students that can be registered
-
-struct Student {
-    string name;
-    string mykad;
-    string password;
-    string studentid;
-};
-
-// Global array to store the registered students
-Student students[MAX_STUDENTS];
-int numStudents = 0;  // Number of students currently registered
-
-// Function to register a new student
-void registerStudent() {
-    if (numStudents >= MAX_STUDENTS) {
-        cout << "Error: Maximum number of students already registered." << endl;
-        return;
-    }
-
-    cout << "Enter name: ";
-    cin >> students[numStudents].name;
-
-    cout << "Enter MyKad Number: ";
-    cin >> students[numStudents].mykad;
-
-
-    cout << "Enter student ID: ";
-    cin >> students[numStudents].studentid;
-
-    cout << "Enter password: ";
-    cin >> students[numStudents].password;
-
-    numStudents++;
-    cout << "Student successfully registered." << endl;
-}
-
-// Function to display a list of all registered students
-void displayStudents() {
-    cout << "Registered students:" << endl;
-    for (int i = 0; i < numStudents; i++) {
-        cout << students[i].name << " (" << students[i].studentid << ")" << endl;
-    }
-}
-
-int main() {
-    while (true) {
-        cout << "Welcome to Administrator Level" << endl;
-        cout << "1. Register a new student" << endl;
-        cout << "2. Display all registered students" << endl;
-        cout << "3. Quit" << endl;
-        cout << "Enter your choice: ";
-
-        int choice;
-        cin >> choice;
-
-        if (choice == 1) {
-            registerStudent();
-        }
-        else if (choice == 2) {
-            displayStudents();
-        }
-        else if (choice == 3) {
-            break;
-        }
-        else {
-            cout << "Invalid choice. Try again." << endl;
-        }
-    }
-
-    return 0;
 }
 
 // statistics of voting result
